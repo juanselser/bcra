5c69a5eca1ff45513250bcf1b48ee27720365cc1
fernandop1987 <fernando.pedroarena@gmail.com>
2025-04-30 11:51:55 -0300

    Update data_fetching.py

--- data_fetching.py
+++ data_fetching.py
@@ -106,6 +106,11 @@ def get_cny(start_date, end_date):
 
 def get_merval(start_date, end_date):
     merval = yf.download("^MERV", start=start_date, end=end_date)
+    if merval.empty:
+        st.error("No se pudieron obtener datos de Yahoo Finance (Merval). El DataFrame está vacío.")
+    else:
+        st.success("Datos del Merval obtenidos correctamente.")
+        st.write(merval.head())
     merval_close = merval.xs("Close", axis=1, level="Price")
     merval = merval_close.rename(columns={"^MERV": "merval_ars"}).reset_index()
     merval = merval.rename(columns={"Date": "fecha"})
