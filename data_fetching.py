99caca087140a392886e6cef7e01d12d3589430d
fernandop1987 <fernando.pedroarena@gmail.com>
2025-04-30 01:30:19 -0300

    Update data_fetching.py

--- data_fetching.py
+++ data_fetching.py
@@ -105,14 +105,16 @@ def get_cny(start_date, end_date):
     return df_cny
 
 def get_merval(start_date, end_date):
-    merval = yf.download("^MERV", start=start_date, end=end_date).reset_index()
+    merval = yf.download("^MERV", start=start_date, end=end_date)
+    merval_close = merval.xs("Close", axis=1, level="Price")
+    merval = merval_close.rename(columns={"^MERV": "merval_ars"}).reset_index()
     merval = merval.rename(columns={"Date": "fecha"})
-    merval = merval[["fecha", "Adj Close"]].rename(columns={"Adj Close": "merval_ars"})
-    merval = merval.dropna().drop_duplicates(subset=["fecha"])
+    merval = merval.rename(columns={"Date": "fecha"})
+    #merval = merval.dropna().drop_duplicates(subset=["fecha"])
 
     df_usd_blue = get_usd_blue()
     df_usd_blue = df_usd_blue[df_usd_blue["fecha"].between(start_date, end_date)]
-    df_usd_blue = df_usd_blue.dropna().drop_duplicates(subset=["fecha"])
+    #df_usd_blue = df_usd_blue.dropna().drop_duplicates(subset=["fecha"])
 
     df_merval = pd.merge(merval, df_usd_blue, on="fecha", how="inner")
     df_merval["merval_usd"] = df_merval["merval_ars"] / df_merval["usd_blue"]
